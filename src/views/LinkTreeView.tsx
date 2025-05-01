a28eba000449a0864c32c916e29e92560163871b
RogelioRamir <rogelio.ramirez@inchcape.com>
2025-04-30 20:49:03 -0500

    validacion para agregar link de redes sociales

--- src/views/LinkTreeView.tsx
+++ src/views/LinkTreeView.tsx
@@ -1,16 +1,47 @@
 import { useState } from "react";
 import { social } from "../data/social";
+import DevTreeInput from "../components/DevTreeInput";
+import { isValidURL } from "../utils";
+import { toast } from "sonner";
 
 
 const LinkTreeView = () => {
 
-    const [ devTreeLinks, setDevTreeLinks ] = useState(social)
+    const [devTreeLinks, setDevTreeLinks] = useState(social)
+    console.log("🚀 ~ LinkTreeView ~ devTreeLinks:", devTreeLinks)
 
+    const handleUrlChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const selectedName = e.target.name
+        const textValue = e.target.value
+        const modifedSocialURL = devTreeLinks.map(item => item.name === selectedName ? (
+            { ...item, url: textValue }
+        ) : item)
+        setDevTreeLinks(modifedSocialURL)
+    }
 
+    const handleEnableLink = (socialNetwork: string) => {
+        const changeEnable = devTreeLinks.map(item => {
+            if(item.name === socialNetwork) {
+                if(isValidURL(item.url)) {
+                    return { ...item, enabled: !item.enabled }
+                } else {
+                    toast.error('URL no válida')
+                }
+            }
+            return item
+        })
+        setDevTreeLinks(changeEnable)
+    }
 
     return (
         <>
-            linktree
+            <div>
+                {
+                    devTreeLinks.map(item => (
+                        <DevTreeInput key={item.name} item={item} handleUrlChange={handleUrlChange} handleEnableLink={handleEnableLink} />
+                    ))
+                }
+            </div>
         </>
     )
 }
